b228d2be7edc236aaf9fb5bb468591546b5306d1
Lameiras <afplameiras@gmail.com>
2021-05-20 00:38:42 +0100

    Criadas funçoes CRUD para as tabelas da base de dados

--- app/src/main/java/com/example/covidnaeuropa/TabelaDasMortes.kt
+++ app/src/main/java/com/example/covidnaeuropa/TabelaDasMortes.kt
@@ -1,6 +1,7 @@
 package com.example.covidnaeuropa
 
 import android.content.ContentValues
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
@@ -16,6 +17,30 @@ class TabelaDasMortes(db: SQLiteDatabase) {
                 "FOREIGN KEY(" + CAMPO_ID_PAIS + ") " +
                 "REFERENCES " + TabelaDePaises.NOME_TABELA +
                 ")")
+    }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(TabelaDePaises.NOME_TABELA, null, values)
+    }
+
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(TabelaDePaises.NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(TabelaDePaises.NOME_TABELA, whereClause, whereArgs)
+    }
+
+
+    fun query(
+            columns: Array<String>,
+            selection: String,
+            selectionArgs: Array<String>,
+            groupBy: String,
+            having: String,
+            orderBy: String): Cursor? {
+        return db.query(TabelaDePaises.NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
     }
 
     companion object{
